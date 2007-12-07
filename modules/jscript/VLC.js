dd1715fe8ebc8a2300c27276fe92901d8b039ffd
jose <jose@fc4c6f4e-0ff0-11de-8c37-e1423f7a4931>
2007-12-07 14:53:12 +0000

    typo
    git-svn-id: https://phoneyc.googlecode.com/svn/phoneyc/trunk@1100 fc4c6f4e-0ff0-11de-8c37-e1423f7a4931

--- modules/jscript/VLC.js
+++ modules/jscript/VLC.js
@@ -9,12 +9,12 @@ function VLC() {
 	}
 	this.setVariable=function(arg0, arg1) {
 		if ((arg0.length > 255) || (arg1.length > 255)) {
-			add_alert("VLC setVariable() overflow"):
+			add_alert("VLC setVariable() overflow");
 		}
 	}
 	this.addTarget=function(arg0, arg1, arg2, arg3) {
 		if ((arg0.length > 255) || (arg1.length > 255) || (arg2.length > 255) || (arg3.length > 255)) {
-			add_alert("VLC addTarget() overflow"):
+			add_alert("VLC addTarget() overflow");
 		}
 	}
 }
