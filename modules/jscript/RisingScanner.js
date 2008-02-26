82420c8e3f002de7b81c9c7568cbf259d072f98c
jose <jose@fc4c6f4e-0ff0-11de-8c37-e1423f7a4931>
2008-02-26 14:40:59 +0000

    [phoneyc] support for Rising Online Virus Scanner Web Scan ActiveX Control
    git-svn-id: https://phoneyc.googlecode.com/svn/phoneyc/trunk@1189 fc4c6f4e-0ff0-11de-8c37-e1423f7a4931

---
+++ modules/jscript/RisingScanner.js
@@ -0,0 +1,9 @@
+// Rising Online Virus Scanner Web Scan ActiveX Control
+// CVE-NOMATCH
+
+function RisingScanner() {
+    this.BaseURL='';
+    this.UpdateEngine=function() {
+	add_alert('Rising Scanner will download and handle ' + this.BaseURL);
+    }
+}
