b4b33cedc98afb437be99cb35456d2ce69632a27
jose <jose@fc4c6f4e-0ff0-11de-8c37-e1423f7a4931>
2008-08-13 13:44:06 +0000

    [phoneyc] save files even if clam isn't running (bug); dump links (for w); one shot (for w, d)
    git-svn-id: https://phoneyc.googlecode.com/svn/phoneyc/trunk@1291 fc4c6f4e-0ff0-11de-8c37-e1423f7a4931

--- honeywalk.py
+++ honeywalk.py
@@ -16,6 +16,8 @@ HoneyClient spider tool
 OPTIONS:
     -d 		output a Graphviz directed graph
 		of URLs walked
+    -D          dump alist of URLs on the page
+    -1          only analyze the one URL
 """ % progname
     print usagestr
     sys.exit(1)
@@ -27,12 +29,11 @@ def scan(body):
 def clamscan(body):
     m = md5.md5(body).hexdigest()
     if len(body) == 0: return '-'
-    try:
-        os.stat('/tmp/clamd')
-    except OSError: return m, '!!!'
     f = open('/tmp/hc-walk-scan-%s' % m, 'wb')
     f.write(body)
     f.close()
+    try: os.stat('/tmp/clamd')
+    except OSError: return m, '!!!'
     o, i, e = popen2.popen3('/usr/local/bin/clamdscan /tmp/hc-walk-scan-%s' % m)
     try: r = o.read().split('\n')[0].split(':')[1].strip()
     except IndexError: r = '???'
@@ -41,7 +42,7 @@ def clamscan(body):
     e.close()
     return m, r
 
-def walk(hc, URL, referrer, visited, depth):
+def walk(hc, URL, referrer, visited, depth, options):
     if referrer and not depth.has_key(URL): 
         depth[URL] = depth[referrer] + 1
     if depth[URL] > hc.maxdepth: return
@@ -72,31 +73,41 @@ def walk(hc, URL, referrer, visited, depth):
             break
     print js, 
     print scan(js[0])
+    if options['do_list']:
+	print URL
+	for l in hc.pages[URL].links:
+	    print ' --> %s' % l
     #print hc.vbs_eval(url=URL, html=page.contents)
     #vbs = hc.vbs_eval(URL)
+    if options['do_one']: return
     for url in hc.pages[URL].links:
         if url not in visited and url.startswith('http'):
             host = url.split('/')[2]
             if not hc.ignored(host):
-                walk(hc, url, URL, visited, depth)
+                walk(hc, url, URL, visited, depth, options)
 
 def main(argv):
-    try: opts, args = getopt.getopt(argv[1:], 'dh')
+    try: opts, args = getopt.getopt(argv[1:], 'dD1h')
     except: usage(sys.argv[0])
 
-    do_dot = False
+    options = {}
+    options['do_dot'] = False
+    options['do_one'] = False
+    options['do_list'] = False
     for o, a in opts:
-        if o == '-d': do_dot = True
+        if o == '-d': options['do_dot'] = True
+	elif o == '-D': options['do_list'] = True
+	elif o == '-1': options['do_one'] = True
         else: usage(sys.argv[0])
     url = args[0]
 
     start = time.time()
     print 'HoneyWalk started at %s UTC' % time.asctime(time.gmtime())
-    try: walk(hc, url, False, [], {url: 0})
+    try: walk(hc, url, False, [], {url: 0}, options)
     except KeyboardInterrupt: print ''
     end = time.time()
 
-    if do_dot:
+    if options['do_dot']:
         print 'walked %d URLs in %f seconds' % (hc.cnt, end-start)
         print '\n\ndigraph "%s" {' % url
         print '%s\n}' % '\n'.join(hc.digraph)
