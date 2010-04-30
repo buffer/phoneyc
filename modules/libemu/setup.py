e8c6eb0deb9eb9db1130394b322ed0d48b7fec1b
jose.monkey.org <jose.monkey.org@fc4c6f4e-0ff0-11de-8c37-e1423f7a4931>
2010-04-30 15:49:56 +0000

    more flexible, from yanceyslide
    git-svn-id: https://phoneyc.googlecode.com/svn/phoneyc/trunk@1575 fc4c6f4e-0ff0-11de-8c37-e1423f7a4931

--- modules/libemu/setup.py
+++ modules/libemu/setup.py
@@ -4,13 +4,13 @@ from distutils.core import setup, Extension
 
 libemu = Extension('libemu',
                     sources = ['libemu_module.c'],
-                    include_dirs = ['/opt/libemu/include'],
-                    library_dirs = ['/opt/libemu/lib/libemu'],
+                    include_dirs = ['@includedir@', '../../include'],
+                    library_dirs = ['@libdir@', '../../src/.libs'],
                     libraries = ['emu'],
                     )
 
 setup (name = 'libemu',
-       version = '0.2.0',
+       version = '@VERSION@',
        description = 'Python interface to the libemu x86 emulator.',
        author = 'Z. Chen, Georg Wicherski',
        author_email = 'czj.pub@gmail.com, gw@mwcollect.org',
