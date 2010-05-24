f80dcb727e1862a4f7351be21551be5f418f1ea9
angelo.dellaera <angelo.dellaera@fc4c6f4e-0ff0-11de-8c37-e1423f7a4931>
2010-05-24 13:52:28 +0000

    New libemu setup.py script (no hardcoded paths)
    git-svn-id: https://phoneyc.googlecode.com/svn/phoneyc/trunk@1584 fc4c6f4e-0ff0-11de-8c37-e1423f7a4931

--- modules/libemu/setup.py
+++ modules/libemu/setup.py
@@ -1,18 +1,22 @@
-#!/usr/bin/python
+#!/usr/bin/env python
+from distutils.core import setup
+from distutils.extension import Extension
+import commands
 
-from distutils.core import setup, Extension
+def pkgconfig(*packages, **kw):
+    flag_map = {'-I': 'include_dirs', '-L': 'library_dirs', '-l': 'libraries'}
+    for token in commands.getoutput("pkg-config --libs --cflags %s" % ' '.join(packages)).split():
+        kw.setdefault(flag_map.get(token[:2]), []).append(token[2:])
+    return kw
 
-libemu = Extension('libemu',
-                    sources = ['libemu_module.c'],
-                    include_dirs = ['@includedir@', '/opt/libemu/include'],
-                    library_dirs = ['@libdir@', '/opt/libemu/lib', '/opt/libemu/lib/libemu/'],
-                    libraries = ['emu'],
-                    )
-
-setup (name = 'libemu',
-       version = '@VERSION@',
-       description = 'Python interface to the libemu x86 emulator.',
-       author = 'Z. Chen, Georg Wicherski',
-       author_email = 'czj.pub@gmail.com, gw@mwcollect.org',
-       url = 'http://code.google.com/p/phoneyc/, http://libemu.mwcollect.org/',
-       ext_modules = [libemu])
+setup(
+    name = 'libemu',
+    version = '@VERSION@',
+    description = 'Python interface to the libemu x86 emulator',
+    author = 'Z. Chen, Georg Wicherski',
+    author_email = 'czj.pub@gmail.com, gw@mwcollect.org',
+    url = 'http://code.google.com/p/phoneyc/, http://libemu.mwcollect.org/',
+    ext_modules=[
+        Extension("libemu_module", ["libemu_module.c"],**pkgconfig('libemu')),
+    ],
+)
